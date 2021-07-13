a36b6b277e305676e89b4d3fe66a60eceeeb90b3
Alina Hambaryan <alina.hambaryan@doktor.se>
2021-07-13 12:46:59 +0200

    Initial Commit

---
+++ AuthenticationSample/AuthenticationSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationSampleApp.swift
+//  AuthenticationSample
+//
+//  Created by A H on 2021-07-13.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
